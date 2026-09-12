a4acc8e698cff98a95576bf973eb7d992fc784c5
vg4tz2sm4y-stack <vg4tz2sm4y@privaterelay.appleid.com>
2026-09-13 05:41:55 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE = 'study-quest-v1';
+const ASSETS = ['./study-quest.html', './manifest.webmanifest', './quest-emblem.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
