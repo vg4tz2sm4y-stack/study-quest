d3f94277828fb666aeb6840d29a12820ed8d2a00
vg4tz2sm4y-stack <vg4tz2sm4y@privaterelay.appleid.com>
2026-09-13 07:15:03 +0900

    Update cache version and asset file names

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'study-quest-v1';
-const ASSETS = ['./study-quest.html', './manifest.webmanifest', './quest-emblem.svg'];
+const CACHE = 'study-quest-v2';
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
